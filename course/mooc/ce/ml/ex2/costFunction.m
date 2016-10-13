42e135608aa6d95d8c84b326d913f8384ca32e99
rriyadd <rriyadd@yahoo.co.uk>
2016-10-13 15:04:47 -0400

    ex3

--- course/mooc/ce/ml/ex2/costFunction.m
+++ course/mooc/ce/ml/ex2/costFunction.m
@@ -9,14 +9,14 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
+grad = zeros(size(theta)); 
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-%
+% 
 % Note: grad should have the same dimensions as theta
 %
 
